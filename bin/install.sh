cf943272067588167d7e406a65e7178596e65bbb
Naresh Bansal <nareshbansal225@gmail.com>
2021-03-28 19:39:34 +0530

    updated url for help binary

--- bin/install.sh
+++ bin/install.sh
@@ -1,20 +1,19 @@
-echo "####################### Installing ansible dependencies ##############################"
+echo "####################### Installing ansible dependencies ##############################\n\n"
 
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install --yes ansible
+sudo apt install --yes wget ansible
 
-echo "################ updating help binary 'vn' for getting deploy command ################"
+echo "################ updating help binary 'vn' for getting deploy command ################\n\n"
 
-sudo cp ./vn /usr/local/bin/vn
+sudo wget https://raw.githubusercontent.com/naresh335/ansible-virutalhost-multiPHP/main/bin/vh -O /usr/local/bin/vh
+sudo chmod +x /usr/local/bin/vh
 
 echo "################### cloning virtualhost with MultiPHP repo ###########################"
 
+rm -rf ~/vh
 git clone https://gitlab.com/user445/ansible-vh.git ~/vh
-
-echo "copy & modify this command to create virtual host \n\n"
-
+echo "\n\ncopy & modify this command to create virtual host or run '$(tput bold)vh -h$(tput sgr0)' for getting this command during any session\n\n"
 echo 'cd ~/vh && ansible-playbook setup.yaml -K --extra-vars "php_version=7.3 user=dev directory=projects url=app.test'
-
 cd ~/vh && exec ${SHELL}
